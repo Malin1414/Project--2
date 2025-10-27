133b9b2408765f8c3b6881ba88ff5f87f99d84f3
KithmiPabodha <kithmipabodha@gmail.com>
2025-10-28 00:23:57 +0530

    staff-home.ts

--- src/app/staff-home/staff-home.ts
+++ src/app/staff-home/staff-home.ts
@@ -20,6 +20,21 @@ export class StaffHome implements OnInit {
   departments: any[] = [];
   batches: any[] = [];
 
+  defaultNotices = [
+    { date: '23-May-2025', text: 'The Faculty of Urban and Aquatic Bioresource held its orientation program for the new student intake on May 21, 2025, at the faculty premises.' },
+    { date: '01-Jun-2025', text: 'Notice 2' },
+    { date: '11-Jun-2025', text: 'Notice 3' },
+    { date: '18-Jun-2025', text: 'Notice 4' },
+    { date: '21-Jun-2025', text: 'Notice 5' },
+    { date: '22-Jun-2025', text: 'Notice 6' },
+    { date: '26-Jun-2025', text: 'Notice 7' },
+    { date: '02-Jul-2025', text: 'Notice 8' },
+    { date: '08-Jul-2025', text: 'Notice 9' },
+    { date: '10-Jul-2025', text: 'Notice 10' },
+    { date: '12-Jul-2025', text: 'Notice 11' },
+    { date: '15-Jul-2025', text: 'Notice 12' }
+  ];
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -28,9 +43,17 @@ export class StaffHome implements OnInit {
 
   loadNotices() {
     this.http.post<any>('http://localhost:8000/api/notices/get', {})
-      .subscribe(response => {
-        if (response.success) {
-          this.notices = response.notices;
+      .subscribe({
+        next: (response) => {
+          if (response.success && response.notices && response.notices.length > 0) {
+           this.notices = response.notices;
+          } else {
+            this.notices = this.defaultNotices;
+          }
+        },
+        error: (err) => {
+          console.error('Error loading notices:', err);
+          this.notices = this.defaultNotices;
         }
       });
   }
